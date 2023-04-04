7e32124ba4dd60c3221212d7aa01dc3af757d0c4
Sahil Kamath <sahilkamath0108@gmail.com>
2023-04-05 00:04:35 +0530

    all about exercises

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require("cors")
 const bodyParser = require("body-parser")
 
 const userRoutes = require("./routes/userRoutes")
+const exerciseRoutes = require("./routes/exerciseRoutes")
 
 const app = express()
 app.use(bodyParser.json());
@@ -13,6 +14,9 @@ app.use(express.json())
 
 // user 
 app.use('/user',userRoutes)
+
+//exercises
+app.use("/exercise", exerciseRoutes)
 
 
 app.listen(process.env.PORT || 3001, () => console.log(`Listening on port 3001`))
