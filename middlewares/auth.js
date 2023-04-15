f5d4099e3591173ec3e12690eaa7c3a594458811
msdhatrak25@gmail.com <msdhatrak25@gmail.com>
2023-04-15 21:01:47 +0530

    backend auth fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,6 @@
-const JWT = require('jsonwebtoken');
-require('dotenv').config();
-const UserSchema = require('../models/user');
+const JWT = require("jsonwebtoken");
+require("dotenv").config();
+const UserSchema = require("../models/user");
 
 async function verifyToken(req, res, next) {
     try {
@@ -8,47 +8,47 @@ async function verifyToken(req, res, next) {
         const token = authHeader && authHeader.split(" ")[1];
 
         if (!token)
-        return res.status(401).send({ 
-            success: false, 
-            message: 'No token provided.' 
-        });
-    
+            return res.status(401).send({
+                success: false,
+                message: "No token provided.",
+            });
+
         const decode = JWT.verify(token, process.env.SECRET_KEY);
-        console.log(decode);
         req.user = await UserSchema.findById({ _id: decode.id });
 
-        if (!req.user)
-        {
+        if (!req.user) {
             res.status(404).json({
                 success: false,
-                error: "Wrong credentials"
-              });      
+                error: "Wrong credentials",
+            });
         }
 
         next();
     } catch (error) {
-        return res.status(500).json({ 
-            success: false, 
-            message: error.message 
+        return res.status(500).json({
+            success: false,
+            message: error.message,
         });
     }
 }
 
-module.exports =  {
+function signAccessToken(userid) {
+    return new Promise((resolve, reject) => {
+        const payload = {
+            id: userid,
+        };
+        const secret = process.env.SECRET_KEY;
+        const options = {
+            expiresIn: "7d",
+        };
+        JWT.sign(payload, secret, options, (err, token) => {
+            if (err) return reject(err);
+            resolve(token);
+        });
+    });
+}
+
+module.exports = {
     verifyToken,
-    signAccessToken: (userid) => {
-        return new Promise((resolve, reject) => {
-            const payload = {
-                id: userid
-            }
-            const secret = process.env.SECRET_KEY
-            const options = {
-                expiresIn: "1h"
-            }
-            JWT.sign(payload, secret, options, (err, token) => {
-                if (err) return reject(err)
-                resolve(token)
-            })
-        })
-    }
+    signAccessToken,
 };
--- models/user.js
+++ models/user.js
@@ -2,110 +2,123 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
-require('dotenv').config();
+require("dotenv").config();
 
-const userSchema = new Schema({
-    fName: {
-        type: String,
-        required: true,
-        validate(value){
-            if (!(/^[A-Za-z\s]*$/.test(value))) {
-                throw new Error("Name should contain only alphabets and spaces");
-            }
-        }
+const userSchema = new Schema(
+    {
+        fName: {
+            type: String,
+            required: true,
+            validate(value) {
+                if (!/^[A-Za-z\s]*$/.test(value)) {
+                    throw new Error(
+                        "Name should contain only alphabets and spaces"
+                    );
+                }
+            },
+        },
+        lName: {
+            type: String,
+            validate(value) {
+                if (!/^[A-Za-z\s]*$/.test(value)) {
+                    throw new Error(
+                        "Name should contain only alphabets and spaces"
+                    );
+                }
+            },
+        },
+        role: {
+            type: String,
+            enum: ["user", "doctor"],
+            default: "user",
+        },
+        pfp: {
+            type: String,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error("Email is invalid!");
+                }
+            },
+        },
+        phone: {
+            type: Number,
+            required: true,
+            unique: true,
+        },
+        dob: Date,
+        gender: {
+            type: String,
+        },
+        location: String, // City
+        password: {
+            type: String,
+            required: true,
+        },
+        aadhar: {
+            type: Number,
+            min: 100000000000, // Coz 12 digits
+            max: 999999999999,
+        },
+        medicalFiles: [
+            {
+                name: String,
+                type: Buffer,
+            },
+        ],
+        medicalFileCount: {
+            type: Number,
+            default: 0,
+        },
+        insuranceFiles: [
+            {
+                name: String,
+                type: Buffer,
+            },
+        ],
+        insuranceFileCount: {
+            type: Number,
+            default: 0,
+        },
+        height: Number, // only meters
+        weight: Number, // unit is Kg
+        health_score: Number,
+        period_lastDay: Date, // not sure
+        period_how_long: Number,
+        period_mc_duration: Number,
+        period_dates: [
+            {
+                type: Date, // unsure
+            },
+        ],
+        pill_reminder: [
+            {
+                name: Date,
+                duration: Number,
+            },
+        ],
+        OTP: {
+            type: Number,
+        },
     },
-    lName: {
-        type: String,
-        validate(value){
-            if (!(/^[A-Za-z\s]*$/.test(value))) {
-                throw new Error("Name should contain only alphabets and spaces");
-            }
-        }
-    },
-    role: {
-        type: String,
-        enum: ["user","doctor"],
-        default: "user"
-    },
-    pfp: {
-        type: String,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error("Email is invalid!");
-            }
-        }
-    },
-    phone: {
-        type: Number,
-        required: true,
-        unique: true
-    },
-    dob: Date,
-    gender: {
-        type: String,
-    },
-    location: String, // City
-    password: {
-        type: String,
-        required: true
-    },
-    aadhar: {
-        type: Number,
-        min: 100000000000,  // Coz 12 digits
-        max: 999999999999
-    },
-    medicalFiles: [{
-        name: String,
-        type: Buffer
-    }],
-    medicalFileCount: {
-        type: Number,
-        default: 0
-    },
-    insuranceFiles: [{
-        name: String,
-        type: Buffer
-    }],
-    insuranceFileCount: {
-        type: Number,
-        default: 0
-    },
-    height: Number,  // only meters
-    weight: Number,  // unit is Kg
-    health_score: Number,
-    period_lastDay: Date, // not sure
-    period_how_long: Number,
-    period_mc_duration: Number,
-    period_dates: [{
-        type: Date // unsure
-    }],
-    pill_reminder: [{
-        name: Date,
-        duration: Number
-    }],
-    OTP: {
-        type : Number,
-    }
-}, {timestamps: true});
-
+    { timestamps: true }
+);
 
 // Hashing the password
-userSchema.pre("save", async function(next) {
-    try{
-        const salt = await bcrypt.genSalt(10)
-        const hashedPass = await bcrypt.hash(this.password, salt)
-        this.password = hashedPass
-        next()
-    }catch(error){
-        console.log(error)
+userSchema.pre("save", async function (next) {
+    try {
+        const salt = await bcrypt.genSalt();
+        const hashedPass = await bcrypt.hash(this.password, salt);
+        this.password = hashedPass;
+        next();
+    } catch (error) {
+        console.log(error);
     }
-})
+});
 
-
-const UserSchema = mongoose.model("user", userSchema)
-module.exports = UserSchema
+const UserSchema = mongoose.model("user", userSchema);
+module.exports = UserSchema;
