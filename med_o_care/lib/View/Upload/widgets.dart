bf594d662b4faadaf118b8ab98e6b08275a91d84
Vatsal Shah <vsshah2003@gmail.com>
2023-04-27 14:35:47 +0530

    Auth integrated and UI complete

---
+++ med_o_care/lib/View/Upload/widgets.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:med_o_care/Constant/colors.dart';
+
+class UploadFileWidget extends StatefulWidget {
+  final String title;
+  final String type;
+  const UploadFileWidget({super.key, required this.title, required this.type});
+
+  @override
+  State<UploadFileWidget> createState() => _UploadFileWidgetState();
+}
+
+class _UploadFileWidgetState extends State<UploadFileWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //height: MediaQuery.of(context).size.height * 0.14,
+      margin: const EdgeInsets.only(top: 20),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.white,
+          border: Border.all(color: ResourceColors.colorTertiaryLightTheme)),
+      padding: const EdgeInsets.all(19),
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        Column(
+          children: [
+            Text(
+              widget.title,
+              style: TextStyle(fontSize: 18),
+            ),
+            Text(
+              widget.type,
+              style: TextStyle(color: Colors.grey),
+            )
+          ],
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          decoration: BoxDecoration(
+              color: ResourceColors.colorPrimaryLightTheme,
+              borderRadius: BorderRadius.circular(30)),
+          child: Center(
+              child: Text(
+            "Edit",
+            style: TextStyle(color: Colors.white),
+          )),
+        )
+      ]),
+    );
+  }
+}
